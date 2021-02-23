8a1b46dd1350be2face5a9157f8f16243e0af80d
Rafael <rafaelyokoyama925@gmail.com>
2021-02-23 16:22:43 -0300

    dia 2

---
+++ moveit-next/src/pages/index.tsx
@@ -0,0 +1,32 @@
+import { CompletedChallenges } from '../components/CompletedChallenges'
+import { Countdown } from '../components/Countdown'
+import { ExperienceBar } from '../components/ExperienceBar'
+
+
+import Head from 'next/head'
+
+import styles from '../styles/pages/Home.module.css'
+import { Profile } from '../components/Profile.'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile/>
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
+    </div>
+  )
+}
